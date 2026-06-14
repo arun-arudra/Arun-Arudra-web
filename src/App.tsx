c7be2a302f35d3db1e4e2d7765fe8588e7081121
Arun <arun90363@gmail.com>
2026-06-15 00:53:47 +0530

    fix react router basename for /ArunArudra/ subdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/ArunArudra/">
           <ScrollToTopOnRouteChange />
           <Routes>
             <Route path="/" element={<Index />} />
